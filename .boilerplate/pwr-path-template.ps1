05df925591b0af6fd24247b12f12bc4a82953c3c
ReevesA1 <adamrivard87@gmail.com>
2025-08-20 19:51:02 -0400

    d

--- .boilerplate/pwr-path-template.ps1
+++ .boilerplate/pwr-path-template.ps1
@@ -1,11 +1,13 @@
+#! Moving the Dotfile
+
 # Define source and destination paths
-$source = Join-Path $env:USERPROFILE "Downloads\Temp\dotfiles\glance\glance.yml"
-$destination = Join-Path $env:USERPROFILE "Docker\docker-compose\glance\config\glance.yml"
+$source      = Join-Path $env:USERPROFILE "Downloads\Temp\dotfiles\xxxxx\xxxxxx.yml"
+$destination = Join-Path $env:USERPROFILE "Docker\docker-compose\xxxxxx\xxxxxx\xxxxxx.yml"
 
 # Ensure destination directory exists
 $destDir = Split-Path -Parent $destination
 if (-not (Test-Path $destDir)) {
-  New-Item -ItemType Directory -Path $destDir -Force | Out-Null
+    New-Item -ItemType Directory -Path $destDir -Force | Out-Null
 }
 
 # Move the file (overwrite if it exists)
