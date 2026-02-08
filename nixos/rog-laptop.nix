e375769fc333b4cc4615109e481712b3921328d4
ReevesA1 <adamrivard87@gmail.com>
2026-02-08 10:17:15 -0500

    d

--- nixos/rog-laptop.nix
+++ nixos/rog-laptop.nix
@@ -47,6 +47,7 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
+  services.xserver.displayManager.gdm.wayland = false;
 
   # Enable the GNOME Desktop Environment.
   services.displayManager.gdm.enable = true;
