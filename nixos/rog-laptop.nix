5fe4145c104664a0cb695c682915cc87fe36738b
ReevesA1 <adamrivard87@gmail.com>
2026-02-08 15:41:45 -0500

    home manager

--- nixos/rog-laptop.nix
+++ nixos/rog-laptop.nix
@@ -1,14 +1,18 @@
 # Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
+# your system. Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
 
+let
+  # Fetching the 25.11 compatible Home Manager
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz";
+in
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [ 
+    ./hardware-configuration.nix
+    (import "${home-manager}/nixos")
+  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -17,14 +21,7 @@
   #boot.loader.grub.enable = true;
   #boot.loader.grub.device = "/dev/sda";
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
   # Set your time zone.
@@ -32,7 +29,6 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
@@ -45,29 +41,21 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-
-  ########################################
-  ## WAYLAND-ONLY GNOME SETUP
-  ########################################
-
+  # GNOME Setup
   services.displayManager.gdm.enable = true;
   services.displayManager.gdm.wayland = true;
-
   services.desktopManager.gnome.enable = true;
 
-  ########################################
-
-
-  # Configure keymap in X11
+  # Keymap
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
 
-  # Enable CUPS to print documents.
+  # Printing
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
+  # Sound with pipewire
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -75,96 +63,68 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  # Programs
+  programs.firefox.enable = true;
+  programs.dconf.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Nix Settings
+  nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # User Account
   users.users.rocket = {
     isNormalUser = true;
     description = "rocket";
     extraGroups = [ "networkmanager" "wheel" ];
-    createHome = true;
     home = "/home/rocket";
-    packages = with pkgs; [
-    #  thunderbird
-    ];
   };
 
+  # Home Manager Configuration for 'rocket'
+  home-manager.users.rocket = { pkgs, ... }: {
+    home.stateVersion = "25.11";
+    # You can add user-specific home-manager settings here
+  };
 
-  # Enable automatic login for the user.
+  # Automatic login
   services.displayManager.autoLogin.enable = true;
   services.displayManager.autoLogin.user = "rocket";
 
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  # Workaround for GNOME autologin
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
 
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # Enable flakes and nix-command
-  #nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-
-  ##############################################################
-  ################### GNOME DARK MODE ##########################
-  ##############################################################
-  programs.dconf.enable = true;
-
+  # GNOME Customization
   services.desktopManager.gnome.extraGSettingsOverrides = ''
-  [org.gnome.desktop.interface]
-  color-scheme='prefer-dark'
-  gtk-theme='Adwaita-dark'
-  icon-theme='Adwaita'
+    [org.gnome.desktop.interface]
+    color-scheme='prefer-dark'
+    gtk-theme='Adwaita-dark'
+    icon-theme='Adwaita'
   '';
 
-
-
-  ####################################################################
-  ### --- FLATPAK ----------------------------------------------
-  ####################################################################
-
-  # Enable Flatpak support (native NixOS module)
+  # Flatpak
   services.flatpak.enable = true;
-
-  # Enable XDG portals for proper GNOME + Flatpak integration
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
 
-  # Automatically add Flathub repo on boot
   systemd.services.flatpak-repo = {
     wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
     script = ''
-      ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
     '';
   };
 
-  ####################################################################
-  ####################################################################
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # System Packages
   environment.systemPackages = with pkgs; [
-    deskflow
+    #Deskflow
     blackbox-terminal
     curl
     wget
     yad
     git
     just
-
-    # Gnome Extensions
     dconf-editor
     gnome-tweaks
     gnome-extension-manager
@@ -187,36 +147,8 @@
     gnomeExtensions.appindicator
     gnomeExtensions.burn-my-windows
     gnomeExtensions.compiz-windows-effect
-
   ];
 
-
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
-
+  # State Version
+  system.stateVersion = "25.11"; 
 }
