fab2ae66792cc308ba9437cd4d074de062569407
ReevesA1 <adamrivard87@gmail.com>
2026-02-08 13:09:38 -0500

    d

--- nixos/clipcascade-instal.sh
+++ nixos/clipcascade-instal.sh
@@ -6,3 +6,6 @@ cd ClipCascade
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
+
+#Start ClipCascade
+cd $HOME/Downloads/ClipCascade && nohup python3 main.py &> /dev/null &
