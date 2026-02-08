90e4d1622167655262b4054e56e21142a8ddfc4c
ReevesA1 <adamrivard87@gmail.com>
2026-02-08 08:49:53 -0500

    s

--- nixos/easy-start.sh
+++ nixos/easy-start.sh
@@ -1,1 +1,1 @@
-sudo curl -fsSL https://raw.githubusercontent.com/rocketpowerinc/dotfiles/refs/heads/main/nixos/rog-laptop.nix | sudo tee -a /etc/nixos/configuration.nix > /dev/null
+sudo curl -fsSL https://raw.githubusercontent.com/rocketpowerinc/dotfiles/refs/heads/main/nixos/rog-laptop.nix | sudo tee -a /etc/nixos/configuration.nix > /dev/null && sudo nixos-rebuild switch
