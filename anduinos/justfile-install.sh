d52cd049d6002d2dd3efdd4d83f386e32b2ccb58
ReevesA1 <adamrivard87@gmail.com>
2026-02-15 16:16:32 -0500

    s

--- anduinos/justfile-install.sh
+++ anduinos/justfile-install.sh
@@ -10,13 +10,19 @@ trap 'rm -rf "$TMPDIR"' EXIT
 
 git clone --depth 1 https://github.com/rocketpowerinc/dotfiles.git "$TMPDIR"
 
-install -m 644 \
+# Copy justfile to $HHOME
+install -m 777 \
   "$TMPDIR/anduinos/justfile" \
   "$HOME/justfile"
 
-install -m 644 \
+# Copy justfile yad gui to $HOME
+install -m 777 \
   "$TMPDIR/anduinos/justfile-gui.sh" \
   "$HOME/justfile-gui.sh"
+chmod +x "$HOME/justfile-gui.sh"
+
+# Start the Yad GUI
+bash "$HOME/justfile-gui.sh"
 
 
 echo "✔ justfile installed to $HOME/justfile"
