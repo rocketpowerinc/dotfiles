d2368d498f3d1bc9b1e05b49fbb2e3f1d111ce0c
ReevesA1 <adamrivard87@gmail.com>
2026-02-15 16:12:05 -0500

    d

--- anduinos/justfile-install.sh
+++ anduinos/justfile-install.sh
@@ -3,7 +3,7 @@ set -e
 
 
 # Install Dependencies
-sudo apt update && sudo apt install just
+sudo apt update && sudo apt install just yad
 
 TMPDIR="$(mktemp -d)"
 trap 'rm -rf "$TMPDIR"' EXIT
@@ -13,5 +13,10 @@ git clone --depth 1 https://github.com/rocketpowerinc/dotfiles.git "$TMPDIR"
 install -m 644 \
   "$TMPDIR/anduinos/justfile" \
   "$HOME/justfile"
+
+install -m 644 \
+  "$TMPDIR/anduinos/justfile-gui.sh" \
+  "$HOME/justfile-gui.sh"
+
 
 echo "✔ justfile installed to $HOME/justfile"
