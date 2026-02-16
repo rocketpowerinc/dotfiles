c6069953e8b1711a3f8b34a5220053e95d0d6b4e
ReevesA1 <adamrivard87@gmail.com>
2026-02-16 14:49:33 -0500

    s

--- anduinos/justfile-gui.sh
+++ anduinos/justfile-gui.sh
@@ -48,78 +48,81 @@ SEP="<span color='#444444'>─────────────────�
 
 # 4. GUI Definition
 ARGS=(
-  --title="AnduinOS Toolkit"
-  --window-icon="$ICON_PATH"
-  --image="$ICON_PATH"
-  --image-on-top
-  --width=500
-  --height=850
-  --center
-  --form
-  --columns=1
-  --scroll
-  --text-align=center
-  --text="<b><big>AnduinOS Toolkit</big></b>\n"
+    --title="AnduinOS Toolkit"
+    --window-icon="$ICON_PATH"
+    --image="$ICON_PATH"
+    --image-on-top
+    --width=500
+    --height=850
+    --center
+    --form
+    --columns=1
+    --scroll
+    --text-align=center
+    --text="<b><big>AnduinOS Toolkit</big></b>\n"
 
-  --field="${HEADER}CONVENIENCE${FOOTER}:LBL" ""
-  --field="Refresh Justfile:BTN" "bash -c 'run_task refresh-justfile'"
-  --field="Refresh Justfile-GUI:BTN" "bash -c 'run_task refresh-justfile-gui'"
-  --field="$SEP" ""
+    --field="${HEADER}CONVENIENCE${FOOTER}:LBL" ""
+    --field="Refresh Justfile:BTN" "bash -c 'run_task refresh-justfile'"
+    --field="Refresh Justfile-GUI:BTN" "bash -c 'run_task refresh-justfile-gui'"
+    --field="$SEP" ""
 
-  --field="${HEADER}SYSTEM${FOOTER}:LBL" ""
-  --field="Update:BTN" "bash -c 'run_task update'"
-  --field="Upgrade:BTN" "bash -c 'run_task upgrade'"
-  --field="Care:BTN" "bash -c 'run_task care'"
-  --field="Factory Reset:BTN" "bash -c 'run_task factory-reset'"
-  --field="$SEP" ""
+    --field="${HEADER}SYSTEM${FOOTER}:LBL" ""
+    --field="Update:BTN" "bash -c 'run_task update'"
+    --field="Upgrade:BTN" "bash -c 'run_task upgrade'"
+    --field="Care:BTN" "bash -c 'run_task care'"
+    --field="Factory Reset:BTN" "bash -c 'run_task factory-reset'"
+    --field="$SEP" ""
 
-  --field="${HEADER}APT PACKAGES${FOOTER}:LBL" ""
-  --field="Search Package:BTN" "bash -c 'run_task search'"
-  --field="Install Package:BTN" "bash -c 'run_task install'"
-  --field="Remove Package:BTN" "bash -c 'run_task remove'"
-  --field="$SEP" ""
+    --field="${HEADER}APT PACKAGES${FOOTER}:LBL" ""
+    --field="Search Package:BTN" "bash -c 'run_task search'"
+    --field="Install Package:BTN" "bash -c 'run_task install'"
+    --field="Remove Package:BTN" "bash -c 'run_task remove'"
+    --field="$SEP" ""
 
-  --field="${HEADER}FLATPAK PACKAGES${FOOTER}:LBL" ""
-  --field="Flatpak Search:BTN" "bash -c 'run_task flatpak-search'"
-  --field="Flatpak List:BTN" "bash -c 'run_task flatpak-list'"
-  --field="Flatpak Remotes:BTN" "bash -c 'run_task flatpak-remotes'"
-  --field="Flatpak Remove Remote:BTN" "bash -c 'run_task flatpak-remove-remote'"
-  --field="$SEP" ""
+    --field="${HEADER}FLATPAK PACKAGES${FOOTER}:LBL" ""
+    --field="Flatpak Search:BTN" "bash -c 'run_task flatpak-search'"
+    --field="Flatpak List:BTN" "bash -c 'run_task flatpak-list'"
+    --field="Flatpak Remotes:BTN" "bash -c 'run_task flatpak-remotes'"
+    --field="Flatpak Remove Remote:BTN" "bash -c 'run_task flatpak-remove-remote'"
+    --field="$SEP" ""
 
-  --field="${HEADER}BOOTSTRAP${FOOTER}:LBL" ""
-  --field="Bootload Apps:BTN" "bash -c 'run_task bootload-apps'"
-  --field="Tune Gnome:BTN" "bash -c 'run_task tune-gnome'"
-  --field="Tune Nautilus:BTN" "bash -c 'run_task tune-nautilus'"
-  --field="$SEP" ""
+    --field="${HEADER}BOOTSTRAP${FOOTER}:LBL" ""
+    --field="Bootload Apps:BTN" "bash -c 'run_task bootload-apps'"
+    --field="Tune Gnome:BTN" "bash -c 'run_task tune-gnome'"
+    --field="Tune Nautilus:BTN" "bash -c 'run_task tune-nautilus'"
+    --field="Install Wallpaper Shuffle:BTN" "bash -c 'run_task install-wallpaper-shuffle'"
+    --field="Start Wallpaper Shuffle:BTN" "bash -c 'run_task start-wallpaper-shuffle'"
+    --field="Stop Wallpaper Shuffle:BTN" "bash -c 'run_task stop-wallpaper-shuffle'"
+    --field="$SEP" ""
 
-  --field="${HEADER}SYSTEM INFO${FOOTER}:LBL" ""
-  --field="Report Disk:BTN" "bash -c 'run_task report-disk'"
-  --field="Report Memory:BTN" "bash -c 'run_task report-memory'"
-  --field="Report CPU:BTN" "bash -c 'run_task report-cpu'"
-  --field="$SEP" ""
+    --field="${HEADER}SYSTEM INFO${FOOTER}:LBL" ""
+    --field="Report Disk:BTN" "bash -c 'run_task report-disk'"
+    --field="Report Memory:BTN" "bash -c 'run_task report-memory'"
+    --field="Report CPU:BTN" "bash -c 'run_task report-cpu'"
+    --field="$SEP" ""
 
-  --field="${HEADER}EXTERNAL TOOLS${FOOTER}:LBL" ""
-  --field="TuxMate:BTN" "bash -c 'run_task tuxmate'"
-  --field="Linux Toys:BTN" "bash -c 'run_task linux-toys'"
-  --field="LinUtil:BTN" "bash -c 'run_task linutil'"
-  --field="$SEP" ""
+    --field="${HEADER}EXTERNAL TOOLS${FOOTER}:LBL" ""
+    --field="TuxMate:BTN" "bash -c 'run_task tuxmate'"
+    --field="Linux Toys:BTN" "bash -c 'run_task linux-toys'"
+    --field="LinUtil:BTN" "bash -c 'run_task linutil'"
+    --field="$SEP" ""
 
-  --field="${HEADER}ROCKETPOWERINC${FOOTER}:LBL" ""
-  --field="Rocket Dashboard:BTN" "bash -c 'run_task rocket-dashboard'"
-  --field="Clone RocketPowerInc Repos:BTN" "bash -c 'run_task clone-rocketpowerinc'"
-  --field="$SEP" ""
+    --field="${HEADER}ROCKETPOWERINC${FOOTER}:LBL" ""
+    --field="Rocket Dashboard:BTN" "bash -c 'run_task rocket-dashboard'"
+    --field="Clone RocketPowerInc Repos:BTN" "bash -c 'run_task clone-rocketpowerinc'"
+    --field="$SEP" ""
 
-  --field="${HEADER}DESKFLOW &amp; CLIPCASCADE${FOOTER}:LBL" ""
-  --field="Install Deskflow:BTN" "bash -c 'run_task install-df'"
-  --field="Install ClipCascade:BTN" "bash -c 'run_task install-cc'"
-  --field="Restart ClipCascade:BTN" "bash -c 'run_task restart-cc'"
-  --field="Status ClipCascade:BTN" "bash -c 'run_task status-cc'"
-  --field="$SEP" ""
+    --field="${HEADER}DESKFLOW &amp; CLIPCASCADE${FOOTER}:LBL" ""
+    --field="Install Deskflow:BTN" "bash -c 'run_task install-df'"
+    --field="Install ClipCascade:BTN" "bash -c 'run_task install-cc'"
+    --field="Restart ClipCascade:BTN" "bash -c 'run_task restart-cc'"
+    --field="Status ClipCascade:BTN" "bash -c 'run_task status-cc'"
+    --field="$SEP" ""
 
-  --field="${HEADER}TOOLS${FOOTER}:LBL" ""
-  --field="Change Hostname:BTN" "bash -c 'run_task change-hostname'"
+    --field="${HEADER}TOOLS${FOOTER}:LBL" ""
+    --field="Change Hostname:BTN" "bash -c 'run_task change-hostname'"
 
-  --button="Exit Toolkit:1"
+    --button="Exit Toolkit:1"
 )
 
 yad "${ARGS[@]}"
