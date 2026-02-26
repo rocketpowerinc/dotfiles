846acda5692a24042eb60df74a9a75a08112fb82
ReevesA1 <adamrivard87@gmail.com>
2026-02-26 14:45:46 -0500

    f

--- JUSTFILES/anduinos/justfile-install.sh
+++ JUSTFILES/anduinos/justfile-install.sh
@@ -12,12 +12,12 @@ git clone --depth 1 https://github.com/rocketpowerinc/dotfiles.git "$TMPDIR"
 
 # Copy justfile to $HHOME
 install -m 777 \
-  "$TMPDIR/JUSTFILES/anduinos/justfile" \
+  "$TMPDIR/JUSTFILES/AnduinOS/justfile" \
   "$HOME/justfile"
 
 # Copy justfile yad gui to $HOME
 install -m 777 \
-  "$TMPDIR/JUSTFILES/anduinos/justfile-gui.sh" \
+  "$TMPDIR/JUSTFILES/AnduinOS/justfile-gui.sh" \
   "$HOME/justfile-gui.sh"
 chmod +x "$HOME/justfile-gui.sh"
 
